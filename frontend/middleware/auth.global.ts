dcc7c22b0e784ccb74645b440959239934251f53
emekajoseph <josephiwuji@gmail.com>
2026-02-25 11:01:46 +0100

    removed old nuxt

--- frontend/middleware/auth.global.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware((to) => {
-  const token = process.client ? localStorage.getItem('token') : null
-  if (to.path.startsWith('/admin') && !token) {
-    return navigateTo('/auth/login')
-  }
-})
-
