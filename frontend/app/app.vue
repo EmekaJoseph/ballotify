f98e8e5a5642dfbe26a51bbb6416968691490db0
emekajoseph <josephiwuji@gmail.com>
2026-02-25 11:35:08 +0100

    installed fresh taiwind nuxt

---
+++ frontend/app/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-10 flex flex-col items-center gap-4">
+    <h1 class="text-3xl font-bold">Hello daisyUI!</h1>
+    <button class="btn btn-primary">Primary Button</button>
+    <button class="btn btn-secondary">Secondary Button</button>
+    <button class="btn btn-accent">Accent Button</button>
+    <button class="btn btn-ghost">Ghost Button</button>
+    <button class="btn btn-link">Link Button</button>
+  </div>
+</template>
